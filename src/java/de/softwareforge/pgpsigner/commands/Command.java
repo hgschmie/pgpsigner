b5644c32db3fdd0b140d073e9db0a193ebd55add
henning@schmiedehausen.org <henning@schmiedehausen.org@93f0197d-baf1-0310-b0bb-cd2d4de592a2>
2007-05-20 16:21:30 +0000

    Add headers and licensing information.
    git-svn-id: http://svn.softwareforge.de/svn/opensource/pgpsigner/trunk@1001 93f0197d-baf1-0310-b0bb-cd2d4de592a2

--- src/java/de/softwareforge/pgpsigner/commands/Command.java
+++ src/java/de/softwareforge/pgpsigner/commands/Command.java
@@ -1,5 +1,25 @@
 package de.softwareforge.pgpsigner.commands;
 
+/*
+ * Copyright (C) 2007 Henning P. Schmiedehausen
+ *
+ * See the NOTICE file distributed with this work for additional
+ * information
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ *
+ */
+
 import jline.Completor;
 
 import org.apache.commons.cli.CommandLine;
@@ -76,7 +96,7 @@ public interface Command
     boolean matchInteractiveCommand(final String command);
 
     /**
-     * Allows the command to check prerequisites before actually executing the command. Commands that offer 
+     * Allows the command to check prerequisites before actually executing the command. Commands that offer
      * one mode where parameters are processed and another mode where just the command name will display the
      * current setting of a command, should use this method to display the current setting when no arguments
      * are given.
